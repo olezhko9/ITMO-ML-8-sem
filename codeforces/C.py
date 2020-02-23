77b3edcf98dc9b9ee93993401adc99f7fcc6e3f4
Oleg Naumov <oleg.naumov.98@mail.ru>
2020-02-24 01:18:36 +0300

    solved task C

--- codeforces/C.py
+++ codeforces/C.py
@@ -1,37 +1,37 @@
-N, M = 3, 2
-train = [
-    [0, 2, 1],
-    [1, 1, 0],
-    [2, 0, 1]
-]
-
-query = [0, 0]
-distance_type = "euclidean"
-kernel_type = "gaussian"
-window_type = "variable"
-h = 2
+# N, M = 3, 2
+# train = [
+#     [0, 2, 1],
+#     [1, 1, 0],
+#     [2, 0, 1]
+# ]
+#
+# query = [0, 0]
+# distance_type = "euclidean"
+# kernel_type = "gaussian"
+# window_type = "variable"
+# h = 2
 
 import math
 
-# N, M = map(int, input().split(' '))
-#
-# train = []
-# for i in range(N):
-#     train.append(list(map(int, input().split(' '))))
-#
-# query = list(map(int, input().split(' ')))
-# distance_type = input()
-# kernel_type = input()
-# window_type = input()
-# h = int(input())
+N, M = map(int, input().split(' '))
+
+train = []
+for i in range(N):
+    train.append(list(map(int, input().split(' '))))
+
+query = list(map(int, input().split(' ')))
+distance_type = input()
+kernel_type = input()
+window_type = input()
+h = int(input())
 
 
 def manhattan(v1, v2):
-    return sum([abs(a-b) for a, b in zip(v1, v2)])
+    return sum([abs(a - b) for a, b in zip(v1, v2)])
 
 
 def euclidean(v1, v2):
-    return sum([(a - b)**2 for a, b in zip(v1, v2)])**(1/2)
+    return sum([(a - b) ** 2 for a, b in zip(v1, v2)]) ** (1 / 2)
 
 
 def chebyshev(v1, v2):
@@ -47,7 +47,7 @@ distance_f = {
 
 def uniform(u):
     if abs(u) < 1:
-        return 1/2
+        return 1 / 2
     return 0
 
 
@@ -59,30 +59,30 @@ def triangular(u):
 
 def epanechnikov(u):
     if abs(u) < 1:
-        return 3/4 * (1 - u*u)
+        return 3 / 4 * (1 - u * u)
     return 0
 
 
 def quartic(u):
     if abs(u) < 1:
-        return 15/16 * (1 - u*u)**2
+        return 15 / 16 * (1 - u * u) ** 2
     return 0
 
 
 def triweight(u):
     if abs(u) < 1:
-        return 35/32 * (1 - u*u)**3
+        return 35 / 32 * (1 - u * u) ** 3
     return 0
 
 
 def tricube(u):
     if abs(u) < 1:
-        return 70/81 * (1 - abs(u)**3)**3
+        return 70 / 81 * (1 - abs(u) ** 3) ** 3
     return 0
 
 
 def gaussian(u):
-    return 1/(math.sqrt(2*math.pi)) * math.e**(-0.5*u*u)
+    return 1 / (math.sqrt(2 * math.pi)) * math.e ** (-0.5 * u * u)
 
 
 def cosine(u):
@@ -92,11 +92,11 @@ def cosine(u):
 
 
 def logistic(u):
-    return 1 / (math.e**u + 2 + math.e**(-u))
+    return 1 / (math.e ** u + 2 + math.e ** (-u))
 
 
 def sigmoid(u):
-    return (2/math.pi) * (1 / (math.e**u + math.e**(-u)))
+    return (2 / math.pi) * (1 / (math.e ** u + math.e ** (-u)))
 
 
 kernel_f = {
@@ -112,7 +112,6 @@ kernel_f = {
     "sigmoid": sigmoid,
 }
 
-
 dists = []
 for row in train:
     d = distance_f[distance_type](row[:-1], query)
@@ -123,19 +122,19 @@ weights = []
 dists = sorted(dists, key=lambda x: x[0])
 
 for dist in dists:
-    if window_type == "fixed":
-        u = dist[0] / h if h != 0 else 0
+    if dist[0] != 0 and (h == 0 or (window_type == "variable" and dists[h][0] == 0)):
+        k = 0
     else:
-        u = dist[0] / dists[h][0] if dists[h][0] != 0 else 0
+        if window_type == "fixed":
+            u = dist[0] / h if h != 0 else 0
+        elif window_type == "variable":
+            u = dist[0] / dists[h][0] if dists[h][0] != 0 else 0
 
-    k = kernel_f[kernel_type](u)
+        k = kernel_f[kernel_type](u)
 
     weights.append(k)
     y_weights.append(dist[1] * k)
 
-print("%.10f" % (0.0 if sum(weights) == 0 else sum(y_weights) / sum(weights)))
+print("%.10f" % (sum(row[-1] for row in train) / N if sum(weights) == 0 else sum(y_weights) / sum(weights)))
 
 
-# assert distance.euclidean(query, train[0][:-1]) == euclidean(query, train[0][:-1])
-# assert distance.chebyshev(query, train[0][:-1]) == chebyshev(query, train[0][:-1])
-# assert distance.cityblock(query, train[0][:-1]) == manhattan(query, train[0][:-1])
