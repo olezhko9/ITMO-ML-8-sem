80da551b3d549a765713c2bfb893652d6855c8cc
Oleg Naumov <oleg.naumov.98@mail.ru>
2020-03-02 22:15:05 +0300

    created notebook

--- labs/lab2/main.py
+++ labs/lab2/main.py
@@ -2,7 +2,7 @@ import numpy as np
 from pseudoinverse import PseudoinverseRegressor
 from metric import nrmse, smape
 
-
+# reading data
 train = []
 test = []
 with open('./dataset/1.txt', 'r') as f:
@@ -17,8 +17,8 @@ with open('./dataset/1.txt', 'r') as f:
     for i in range(K):
         test.append(list(map(int, f.readline().split(' '))))
 
-train = np.array(train)
-test = np.array(test)
+train = np.array(train, dtype='float64')
+test = np.array(test, dtype='float64')
 
 X_train = np.append(train[:, :-1], np.ones((N, 1)), axis=1)
 y_train = train[:, -1]
@@ -26,13 +26,17 @@ y_train = train[:, -1]
 X_test = np.append(test[:, :-1], np.ones((K, 1)), axis=1)
 y_test = test[:, -1]
 
-print('train: ', train.shape)
-print('test: ', test.shape)
+print('train: ', (X_train.shape, y_train.shape))
+print('test: ', (X_test.shape, y_test.shape))
 
 
+# PSEUDOINVERSE method
 pseudoinverseRegressor = PseudoinverseRegressor()
 theta = pseudoinverseRegressor.fit(X_train, y_train)
-res = pseudoinverseRegressor.predict(X_test)
+y_pred = pseudoinverseRegressor.predict(X_test)
 
-print(nrmse(y_test, res))
-print(smape(y_test, res))
+print('nrmse: ', nrmse(y_test, y_pred))
+print('smape: ', smape(y_test, y_pred))
+
+print(y_test[:18].flatten())
+print(y_pred[:18].flatten())
